761899bf76f463cbdf4422ac96eea42a8ca9c886
Lê Đức Thắng <leducthang98@gmail.com>
2020-07-01 00:46:21 +0700

    Config JwtResponse

--- src/main/java/com/sapo/JWTDemo/Controller/JwtAuthenticateAPI.java
+++ src/main/java/com/sapo/JWTDemo/Controller/JwtAuthenticateAPI.java
@@ -5,6 +5,7 @@ import com.sapo.JWTDemo.Security.JsonWebToken.JwtAuthenicationResponse;
 import com.sapo.JWTDemo.Security.JsonWebToken.JwtUserDetailsService;
 import com.sapo.JWTDemo.Security.JsonWebToken.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -34,11 +35,9 @@ public class JwtAuthenticateAPI {
             final UserDetails userDetails = userDetailsService
                     .loadUserByUsername(jwtAuthenicationRequest.getUsername());
             final String jwt = jwtTokenUtil.generateToken(userDetails);
-
-            return ResponseEntity.ok(new JwtAuthenicationResponse(jwt));
+            return ResponseEntity.ok(new JwtAuthenicationResponse(jwt,userDetails));
         } catch (BadCredentialsException e) {
-
-            return ResponseEntity.ok("Wrong");
+            return new ResponseEntity<String>("Wrong username or password", HttpStatus.BAD_REQUEST);
         }
 
 
--- src/main/java/com/sapo/JWTDemo/Security/JsonWebToken/JwtAuthenicationResponse.java
+++ src/main/java/com/sapo/JWTDemo/Security/JsonWebToken/JwtAuthenicationResponse.java
@@ -1,14 +1,32 @@
 package com.sapo.JWTDemo.Security.JsonWebToken;
 
 import com.sapo.JWTDemo.DTO.Account;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 public class JwtAuthenicationResponse {
     private final String jwt;
+    private String username;
+    private Collection<GrantedAuthority> authorities;
+
+    public JwtAuthenicationResponse(String jwt, UserDetails userDetails) {
+        this.jwt = jwt;
+        this.username = userDetails.getUsername();
+        authorities = (Collection<GrantedAuthority>) userDetails.getAuthorities();
+
+    }
+
     public String getJwt() {
         return jwt;
     }
 
-    public JwtAuthenicationResponse(String jwt) {
-        this.jwt = jwt;
+    public String getUsername() {
+        return username;
+    }
+
+    public Collection<GrantedAuthority> getAuthorities() {
+        return authorities;
     }
 }
